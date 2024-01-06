edd4e6620b8b04b19dbce917231116978aa7e86b
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2024-01-06 10:42:47 +0530

    import java.util.Scanner; public class Ajju{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String ne=sc.nextLine();         System.out.println(ne);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Ajju{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String ne=sc.nextLine();
+        System.out.println(ne);
+    }
+}
