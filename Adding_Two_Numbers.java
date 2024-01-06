a3aa182cdc6b54a44ac3ecbb98f7339a958762fc
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2024-01-06 10:38:07 +0530

    import java.util.Scanner;
    public class Ajju{
        public static void main(String[] args){
            Scanner sc=new Scanner(System.in);
            int ajay=sc.nextInt();
            int nav=sc.nextInt();
            //System.out.println("HI i am ajay");
            System.out.println(ajay+nav);
            sc.close();
        }
    }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ajju{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int ajay=sc.nextInt();
+        int nav=sc.nextInt();
+        //System.out.println("HI i am ajay");
+        System.out.println(ajay+nav);
+        sc.close();
+    }
+}
