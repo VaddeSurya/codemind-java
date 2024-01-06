a4062d855fc72ebec4cf3cc398fa2ffff93d239a
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2024-01-06 10:55:06 +0530

    import java.util.Scanner; public class Ajju{     public static void main(String[] args){         Scanner se=new Scanner(System.in);         int num=se.nextInt();         System.out.println(((num)*(num-1))/2);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Ajju{
+    public static void main(String[] args){
+        Scanner se=new Scanner(System.in);
+        int num=se.nextInt();
+        System.out.println(((num)*(num-1))/2);
+    }
+}
