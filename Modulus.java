6599c4d6df8b6d8690283b86d994795bea60c74b
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2024-01-06 10:47:00 +0530

    import java.util.Scanner; public class Ajju{     public static void main(String[] args)     {         Scanner se=new Scanner(System.in);         int aj=se.nextInt();         int na=se.nextInt();         System.out.println(aj%na);
        }
    }

---
+++ Modulus.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Ajju{
+    public static void main(String[] args)
+    {
+        Scanner se=new Scanner(System.in);
+        int aj=se.nextInt();
+        int na=se.nextInt();
+        System.out.println(aj%na);
+        
+    }
+}
