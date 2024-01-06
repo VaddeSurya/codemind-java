bb785e28b4fcf9319ea6c873cd4a123315d094a2
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2024-01-06 11:51:22 +0530

    import java.util.Scanner; public class Ak{     public static void main(String[] args)     {         Scanner se=new Scanner(System.in);         int aj=se.nextInt();         int na=se.nextInt();         int sh=se.nextInt();         System.out.println(aj*na*sh*1024);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Ak{
+    public static void main(String[] args)
+    {
+        Scanner se=new Scanner(System.in);
+        int aj=se.nextInt();
+        int na=se.nextInt();
+        int sh=se.nextInt();
+        System.out.println(aj*na*sh*1024);
+    }
+}
