e56040542754d1260ac6303fafb25e2dc9b4a273
VaddeSurya <119840247+VaddeSurya@users.noreply.github.com>
2024-01-06 11:57:28 +0530

    import java.util.Scanner; public class Aju{     public static void main(String[] args)     {         Scanner se=new Scanner(System.in);         int a=se.nextInt();         int b=se.nextInt();         System.out.println(a/b);     } }

---
+++ Division.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Aju{
+    public static void main(String[] args)
+    {
+        Scanner se=new Scanner(System.in);
+        int a=se.nextInt();
+        int b=se.nextInt();
+        System.out.println(a/b);
+    }
+}
